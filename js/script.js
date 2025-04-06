d0a8e3c79dae8f19107666bb45e7d6b214e8464d
Satyamrk <satyamrk18@gmail.com>
2025-04-06 23:25:13 +0530

    html css and js file distributed

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let hamburgerNav = document.getElementById("hamburgerNav");
+      let hamNav = document.getElementById("hamburger-menu");
+      function hamMenu() {
+        //hamburger menu image cance to cancel image code
+        if (hamNav.src.includes("menus.png")) {
+          hamNav.src = "./images/cancel.png";
+        } else {
+          hamNav.src = "./images/menus.png";
+        }
+        //hamburger menu display on small phone screen code
+        if (
+          hamburgerNav.style.display == "none" ||
+          hamburgerNav.style.display == ""
+        ) {
+          hamburgerNav.style.display = "block";
+        } else {
+          hamburgerNav.style.display = "none";
+        }
+      }
+      // Ensure menu is always visible on larger screens
+      function checkScreenSize() {
+        if (window.innerWidth < 601) {
+          hamburgerNav.style.display = "none";
+        } else {
+          hamburgerNav.style.display = "flex";
+        }
+      }
+
+      // Run when browser trigger a resizing a page
+      checkScreenSize();
+
+      // Re-check when window resizes
+      window.onresize = checkScreenSize; // it is a functions call when broeser triggred as a resizing
