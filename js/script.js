11545e7165a5cae8cc93a9402f085e3ad080a725
Satyamrk <satyamrk18@gmail.com>
2025-04-10 12:00:02 +0530

    variable name updated

--- js/script.js
+++ js/script.js
@@ -36,37 +36,44 @@ window.onresize = checkScreenSize; // it is a functions call when broeser triggr
 let started = false;
 
 function startExperienceCounter() {
-  const el = document.getElementById("incremetnal-info-experience");
+  const incrementalExperience = document.getElementById(
+    "incremetnal-info-experience"
+  );
   let i = 1;
   const interval = setInterval(() => {
-    el.innerHTML = `${i}+ <span>Year's Experience</span>`;
+    incrementalExperience.innerHTML = `${i}+ <span>Year's Experience</span>`;
     if (i === 10) clearInterval(interval);
     i++;
   }, 200);
 }
 
 function startProjectCounter() {
-  const el = document.getElementById("incremetnal-info-projects");
+  const incrementalProject = document.getElementById(
+    "incremetnal-info-projects"
+  );
   let i = 1;
   const interval = setInterval(() => {
-    el.innerHTML = `${i}+ <span>Project's Complete</span>`;
+    incrementalProject.innerHTML = `${i}+ <span>Project's Complete</span>`;
     if (i === 100) clearInterval(interval);
     i++;
   }, 20);
 }
 
 function startServiceCounter() {
-  const el = document.getElementById("incremetnal-info-services");
+  const incrementalServices = document.getElementById(
+    "incremetnal-info-services"
+  );
   let i = 1;
   const interval = setInterval(() => {
-    el.innerHTML = `${i}+ <span>Services</span>`;
+    incrementalServices.innerHTML = `${i}+ <span>Services</span>`;
     if (i === 20) clearInterval(interval);
     i++;
   }, 100);
 }
 
 // ✅ Intersection Observer to detect when section is visible
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver(
+  (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting && !started) {
         startExperienceCounter();
@@ -77,11 +84,11 @@ const observer = new IntersectionObserver((entries) => {
     });
   },
   {
-    threshold:0.7, // 50% of element should be visible
+    threshold: 0.7, // 50% of element should be visible
   }
 );
 
 // 👇 Observe the counter section
 observer.observe(document.getElementById("counter-section"));
-//to initialize the the animation 
+//to initialize the the animation
 AOS.init();
